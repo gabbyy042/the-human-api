40a55cb9759620ac44a83368c8a66e9af2207a7a
gabbyy042 <Chukwugabriel24@gmail.com>
2025-11-15 03:20:39 -0800

    Add About page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div className="relative overflow-hidden min-h-screen">
+      <div className="absolute inset-0 bg-gradient-to-b from-[#0a1929] via-[#1e3a8a]/20 to-[#0a1929]" />
+      
+      <div className="relative z-10">
+        <section className="container mx-auto px-4 py-20">
+  
